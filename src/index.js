4de340c6800fa290a26f59ed714573132343afd5
AliaxeiSehiyenia <alexeisergienia@gmail.com>
2021-02-16 02:21:16 +0300

    feat: change function towelSort

--- src/index.js
+++ src/index.js
@@ -1,21 +1,35 @@
 module.exports = function towelSort(matrix) {
     if (matrix && matrix.length != 0) {
-        function Sort(ElementArr) {
+        function Sort(ElementArr, index) {
             var n = ElementArr.length;
-            for (var i = 0; i < n - 1; i++) {
-              for (var j = 0; j < n - 1 - i; j++) {
-                if (ElementArr[j + 1] < ElementArr[j]) {
-                  var t = ElementArr[j + 1];
-                  ElementArr[j + 1] = ElementArr[j];
-                  ElementArr[j] = t;
+            if (index % 2 == 1) { 
+             for (var i = 0; i < n - 1; i++) {
+                for (var j = 0; j < n - 1 - i; j++) {
+                  if (ElementArr[j + 1] > ElementArr[j]) {
+                    var t = ElementArr[j + 1];
+                    ElementArr[j + 1] = ElementArr[j];
+                    ElementArr[j] = t;
+                  }
                 }
               }
             }
+            else {
+             for (var i = 0; i < n - 1; i++) {
+                for (var j = 0; j < n - 1 - i; j++) {
+                  if (ElementArr[j + 1] < ElementArr[j]) {
+                    var t = ElementArr[j + 1];
+                    ElementArr[j + 1] = ElementArr[j];
+                    ElementArr[j] = t;
+                  }
+                }
+              }
+            }
+ 
             return ElementArr;
           }
         
           matrix.forEach(Sort);
-          return matrix;
+          return(matrix);
     }
     else {
         return new Array();
