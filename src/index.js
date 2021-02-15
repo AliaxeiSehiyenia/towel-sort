7a57dae0d5685a93b6eadd850f2739bbae704829
AliaxeiSehiyenia <alexeisergienia@gmail.com>
2021-02-15 23:20:23 +0300

    feat: change function towelSort

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-function towelSort (matrix) {
-    function sortNumbers(a, b) {
-        return a - b;
-    }  
-     matrix.forEach(row => row.sort(sortNumbers));          
-     return matrix;
+module.exports = function towelSort (matrix) {
+
+    function Sort(ElementArr) {                            
+    var n = ElementArr.length;
+    for (var i = 0; i < n-1; i++)
+     { for (var j = 0; j < n-1-i; j++)
+        { if (ElementArr[j+1] < ElementArr[j])
+           { var t = ElementArr[j+1]; ElementArr[j+1] = ElementArr[j]; ElementArr[j] = t; }
+        }
+     }                     
+    return ElementArr;
+    }
+    
+    matrix.forEach(Sort);   
+    return matrix;    
+ 
 }
