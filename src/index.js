4f90a5a9e8427f3d45871f4c42f3c7f41b475855
AliaxeiSehiyenia <alexeisergienia@gmail.com>
2021-02-16 22:54:03 +0300

    feat: change function towelSort

--- src/index.js
+++ src/index.js
@@ -1,38 +1,35 @@
 module.exports = function towelSort(matrix) {
-    if (matrix) {
-        function Sort(ElementArr, index) {
-            var n = ElementArr.length;
-            if (index % 2 == 1) { 
-             for (var i = 0; i < n - 1; i++) {
-                for (var j = 0; j < n - 1 - i; j++) {
-                  if (ElementArr[j + 1] > ElementArr[j]) {
-                    var t = ElementArr[j + 1];
-                    ElementArr[j + 1] = ElementArr[j];
-                    ElementArr[j] = t;
-                  }
-                }
-              }
+  if (matrix) {
+    function Sort(ElementArr, index) {
+      var n = ElementArr.length;
+      if (index % 2 == 1) {
+        for (var i = 0; i < n - 1; i++) {
+          for (var j = 0; j < n - 1 - i; j++) {
+            if (ElementArr[j + 1] > ElementArr[j]) {
+              var t = ElementArr[j + 1];
+              ElementArr[j + 1] = ElementArr[j];
+              ElementArr[j] = t;
             }
-            else {
-             for (var i = 0; i < n - 1; i++) {
-                for (var j = 0; j < n - 1 - i; j++) {
-                  if (ElementArr[j + 1] < ElementArr[j]) {
-                    var t = ElementArr[j + 1];
-                    ElementArr[j + 1] = ElementArr[j];
-                    ElementArr[j] = t;
-                  }
-                }
-              }
-            }
- 
-            return ElementArr;
           }
-        
-          matrix.forEach(Sort);
-          return(matrix);
+        }
+      } else {
+        for (var i = 0; i < n - 1; i++) {
+          for (var j = 0; j < n - 1 - i; j++) {
+            if (ElementArr[j + 1] < ElementArr[j]) {
+              var t = ElementArr[j + 1];
+              ElementArr[j + 1] = ElementArr[j];
+              ElementArr[j] = t;
+            }
+          }
+        }
+      }
+
+      return ElementArr;
     }
-    else {
-        return new Array();
-    }
-  
+
+    matrix.forEach(Sort);
+    return matrix.flat();
+  } else {
+    return new Array();
+  }
 };
