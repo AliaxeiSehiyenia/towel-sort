ba1e3e7bdb51388b3f4b12b15200a672ee43efa2
AliaxeiSehiyenia <alexeisergienia@gmail.com>
2021-02-26 14:50:23 +0300

    feat: change function towelSort

--- src/index.js
+++ src/index.js
@@ -1,35 +1,37 @@
 module.exports = function towelSort(matrix) {
   if (matrix) {
-    function Sort(ElementArr, index) {
-      var n = ElementArr.length;
-      if (index % 2 == 1) {
-        for (var i = 0; i < n - 1; i++) {
-          for (var j = 0; j < n - 1 - i; j++) {
-            if (ElementArr[j + 1] > ElementArr[j]) {
-              var t = ElementArr[j + 1];
-              ElementArr[j + 1] = ElementArr[j];
-              ElementArr[j] = t;
-            }
-          }
-        }
-      } else {
-        for (var i = 0; i < n - 1; i++) {
-          for (var j = 0; j < n - 1 - i; j++) {
-            if (ElementArr[j + 1] < ElementArr[j]) {
-              var t = ElementArr[j + 1];
-              ElementArr[j + 1] = ElementArr[j];
-              ElementArr[j] = t;
-            }
-          }
-        }
-      }
-
-      return ElementArr;
-    }
-
     matrix.forEach(Sort);
     return matrix.flat();
-  } else {
-    return new Array();
-  }
+    /* Метод flat() возвращает новый массив, в котором все элементы вложенных подмассивов 
+    были рекурсивно "подняты" на указанный уровень depth. */
+  } else return [];
 };
+
+function Sort(elementArr, index) {
+    /* Пузырьковая сортировка. 
+    Для реализации такой концепции нам нужны два указателя (два вложенных цикла). 
+    Каждый раз, когда мы выполняем итерацию, верхняя граница уменьшается на единицу, 
+    поскольку мы знаем, что этот элемент уже содержит отсортированное значение. */
+  if (index % 2 == 1) { // Элементы с нечётным индексом сортируются по убыванию.
+    for (let i = 0; i < elementArr.length - 1; i++) {
+      for (let j = 0; j < elementArr.length - 1 - i; j++) {
+        if (elementArr[j + 1] > elementArr[j]) {
+          let t = elementArr[j + 1];
+          elementArr[j + 1] = elementArr[j];
+          elementArr[j] = t;
+        }
+      }
+    }
+  } else { // Элементы с чётным индексом сортируются по возростанию.
+    for (let i = 0; i < elementArr.length - 1; i++) {
+      for (let j = 0; j < elementArr.length - 1 - i; j++) {
+        if (elementArr[j + 1] < elementArr[j]) {
+          let t = elementArr[j + 1];
+          elementArr[j + 1] = elementArr[j];
+          elementArr[j] = t;
+        }
+      }
+    }
+  }
+  return elementArr;
+}
